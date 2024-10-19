c45dc8246faef6c56048a9a956a49becb80fc68d
Landon Harris <landonharris979@gmail.com>
2024-10-19 18:58:53 -0500

    Welcome Screen and Background

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+const colors = ['#ff9a9e', '#fad0c4', '#fbc2eb', '#a18cd1', '#fbc2eb', '#a6c0fe', '#88d3ce'];
+let currentColorIndex = 0;
+
+function changeColor() {
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
+    document.querySelector('.background').style.background = `
+        linear-gradient(to top , rgba(255, 255, 255, 0.4), rgba(0, 0, 0, 0.8)),
+        ${colors[currentColorIndex]}
+    `;
+}
+
+setInterval(changeColor, 4000); // Change color every 4 seconds
