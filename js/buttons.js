d4feafe98cdb639d86306a8b4421b4e5c9c23bc9
Landon Harris <landonharris979@gmail.com>
2024-10-21 17:08:23 -0500

    Organized and animated events

--- js/buttons.js
+++ js/buttons.js
@@ -1,10 +1,32 @@
+// buttons.js
+
+// Function to create animated text drop down effect
+function animateText(button, newText, url) {
+  // Clear existing text
+  button.textContent = '';
+
+  // Create a span for each letter with animation class
+  newText.split('').forEach((char, index) => {
+    let span = document.createElement('span');
+    span.textContent = char;
+    span.style.animationDelay = `${index * 0.1}s`; // Adjust delay as needed
+    span.classList.add('animated-text');
+    button.appendChild(span);
+  });
+
+  // Delay navigation to allow animation to play
+  setTimeout(function() {
+    window.location.href = url;
+  }, newText.length * 100 + 300); // Adjust timing based on animation
+}
+
 const exploreButton = document.getElementById('explore');
 const homeButton = document.getElementById('home');
 
 // Check if elements exist in the DOM before adding event listeners
 if (exploreButton) {
   exploreButton.addEventListener('mousedown', function(event) {
-    window.location.href = "https://landoharris.github.io/Portfolio/about.html";
+    animateText(exploreButton, 'Enjoy', "https://github.com/LandoHarris");
   });
 
   exploreButton.addEventListener('mouseover', function(event) {
@@ -18,7 +40,7 @@ if (exploreButton) {
 
 if (homeButton) {
   homeButton.addEventListener('mousedown', function(event) {
-    window.location.href = "https://landoharris.github.io/Portfolio/index.html";
+    animateText(homeButton, 'Enjoy', "https://landoharris.github.io/Portfolio/index.html");
   });
 
   homeButton.addEventListener('mouseover', function(event) {
